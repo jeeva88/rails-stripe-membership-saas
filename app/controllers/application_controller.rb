22cbcfaca7c6e14fc2fe7133395eae74000e7f5a
jeeva <jeeva.packiaraj@gmail.com>
2012-11-29 22:03:34 -0500

    redirect after logged in to appropriate page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+  def after_sign_in_path_for(resource)
+    case current_user.roles.first.name
+      when 'admin'
+        users_path
+      when 'silver'
+        content_silver_path
+      when 'gold'
+        content_gold_path
+      when 'platinum'
+        content_platinum_path
+      else
+        root_path
+    end
+  end
 
 end
